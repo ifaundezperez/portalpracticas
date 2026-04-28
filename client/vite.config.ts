66511d8064690b28b354acabf7001603ae07e433
Ignacio Faúndez Pérez <i.faundezperez99@gmail.com>
2026-04-28 14:16:40 -0400

    Configuración de proxy para conexión con el backend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Cada vez que en React uses una ruta que empiece con /api, 
+      // Vite la redirigirá automáticamente a tu servidor de Node.
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
